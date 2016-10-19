8a1279e8714bb146ac8aa61bdfaf216efb3d7d98
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2016-10-18 23:13:43 -0500

    changing name again / removing unnecessary files

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='fancySubprocesses',
+    name='subprocessPlus',
     version='0.0.1',
-    description='A set of functions to run parallel or series subprocesses with Python',
+    description='A set of functions to run improved parallel or series subprocesses with Python',
     long_description=readme,
     author='Salvatore Davide Porzio',
     author_email='porziodavide@gmail.com',
     url='https://github.com/sdporzio/fancy-subprocesses',
     license='MIT',
-    packages=['fancySubprocesses'],
+    packages=['subprocPlus'],
     install_requires=[
         'numpy'
     ],
