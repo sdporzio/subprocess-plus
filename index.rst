d4846d51b464cd8929c5acadf6a67abdd22e2cd0
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2016-10-18 20:01:45 -0500

    adding test and docs

---
+++ index.rst
@@ -0,0 +1,22 @@
+.. FancySubprocesses documentation master file, created by
+   sphinx-quickstart on Tue Oct 18 20:00:54 2016.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to FancySubprocesses's documentation!
+=============================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- sample/fancySubprocesses.py
+++ sample/fancySubprocesses.py
@@ -6,12 +6,10 @@ from subprocess import call, Popen
 firstRun = True
 
 def Notifier(cmd,curr,tot,email):
-    """ Append at the end of the command, an extra command to notify the user by mail when the job has finished. """
     cmd += '; echo "Job \'%s\' has finished." | mail -s "Job %i of %i finished" "%s"' %(cmd,curr,tot,email)
     return cmd
 
 def Temp2Perm(temp,perm,name,start,end):
-    """ Saves temporary (temp) log files in permanent (perm) log files. """
     temp.seek(0)
     perm.write('%s\n%s -> %s\n' %(name,start,end))
     perm.write(temp.read())
@@ -19,26 +17,6 @@ def Temp2Perm(temp,perm,name,start,end):
     temp.close()
 
 def FancySeries(cmd,i,njobs,email=False,inName='logIn.log',outName='logOut.log',errName='logErr.log'):
-    """ Runs a series of jobs in series.
-
-    Parameters
-    ----------
-    cmd : string
-        The command to execute.
-    i : int
-        The looping index of the current job
-    njobs : int
-        The number of jobs to execute.
-    email : bool or string, optional (default=False)
-        The email address of the user at which the notification that the job has ended is sent.
-    inName : string
-        The name of the log file containing the stdIn.
-    outName : string
-        The name of the log file containing the stdOut.
-    errName : string
-        The name of the log file containing the stdErr.
-    """
-
     global firstRun
     if firstRun:
         logIn = open(inName, 'w')
@@ -66,28 +44,6 @@ def FancySeries(cmd,i,njobs,email=False,inName='logIn.log',outName='logOut.log',
     Temp2Perm(errl,logErr,cmd,start,end)
 
 def FancyParallel(cmd,i,njobs,procs=procs,email=False,inName='logIn.log',outName='logOut.log',errName='logErr.log'):
-    """ Runs a series of jobs in parallel.
-
-    Parameters
-    ----------
-    cmd : string
-        The command to execute.
-    i : int
-        The looping index of the current job
-    njobs : int
-        The number of jobs to execute.
-    procs : array
-        The array containing all the informations to be processed at the end of the jobs
-        (necessary to maintain the correct order in the log files).
-    email : bool or string, optional (default=False)
-        The email address of the user at which the notification that the job has ended is sent.
-    inName : string
-        The name of the log file containing the stdIn.
-    outName : string
-        The name of the log file containing the stdOut.
-    errName : string
-        The name of the log file containing the stdErr.
-    """
     global firstRun
     if firstRun:
         logIn = open(inName, 'w')
